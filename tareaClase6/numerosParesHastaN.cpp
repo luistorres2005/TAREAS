418244690c9853cf0a1416e91d472248ad7f83b1
luistorres2005 <luistorres.20199@gmail.com>
2026-07-30 21:49:05 -0500

    ejercicio2

--- tareaClase6/numerosParesHastaN.cpp
+++ tareaClase6/numerosParesHastaN.cpp
@@ -1,4 +1,10 @@
 /*
+Nombre del estudiante: TORRES CABEZAS YOBER LUIS
+Fecha: 30/07/2026
+Tema: Nuemeros pares hasta N
+*/
+
+/*
 Ejercicio 1 — Números pares hasta N
 Elaborar un programa en C++ que solicite al usuario un número entero positivo N y
 muestre en pantalla todos los números pares desde 1 hasta N.
--- tareaClase6/sumaNumeros.cpp
+++ tareaClase6/sumaNumeros.cpp
@@ -1,4 +1,11 @@
 /*
+Nombre del estudiante: TORRES CABEZAS YOBER LUIS
+Fecha: 30/07/2026
+Tema: Suma de 5 numeros
+
+*/
+
+/*
 Ejercicio 2 — Suma de 5 números
 Elaborar un programa en C++ que solicite al usuario 5 números enteros y calcule la
 suma total de esos valores.
@@ -6,3 +13,32 @@ Al final, el programa debe mostrar:
 • la suma total
 • y cuántos números fueron ingresados
 */
+
+/*
+E: 5 números enteros
+P: Calcular la suma total de esos valores
+S: la suma total y cuántos números fueron ingresados 
+*/
+
+#include <iostream>
+using namespace std;    
+
+int main() { 
+    int numero;
+    int cantidad = 0; // Acumulador, inicia en 0
+    int suma = 0; // Contador de numeros ingresados, inicia en 0
+
+    cout << "Ingrese 5 numeros enteros:" << endl;
+    for (int i = 1; i <= 5; i++) {
+        cout << " Ingrese el numero " << i << ": ";
+        cin >> numero;
+        suma += numero; //acumula el valor
+        cantidad++; // contar el numero ingresado
+    }
+
+    cout << "\nResultados: " << endl;
+    cout << "La suma total es:  " << suma << endl;
+    cout << "Cantidad de numeros ingresados: " << cantidad << endl;
+
+    return 0; // Termina el programa    
+}
