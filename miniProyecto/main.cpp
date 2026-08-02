a9dd85a42679f1e7faa3750da1959bc82f68e661
luistorres2005 <luistorres.20199@gmail.com>
2026-08-02 17:46:19 -0500

    Ingreso de E/P/S

---
+++ miniProyecto/main.cpp
@@ -0,0 +1,44 @@
+/*
+Nombre del estudiante: TORRES CABEZAS YOBER LUIS
+Fecha: 03/08/2026
+Tema: GymFit Admin
+*/
+
+/*
+GymFit Admin es un programa diseñado para administrar de forma 
+sencilla y eficiente los datos de un gimnasio. 
+Su propósito principal es permitir el registro de 
+los socios, buscar su información, realizar cálculos de control 
+general y ofrecer recomendaciones personalizadas de salud.
+*/
+
+/*
+E/P/S
+
+Entrada:
+    1.Las decisiones en el menú: El número de la opción que queremos elegir.
+    2. Los datos para registrar un socio:
+        Su Nombre completo.
+        Su Edad en años.
+        Su Peso en kilogramos.
+    3. El nombre de la persona que queremos consultar.    
+
+Proceso:
+    1. Revisa que no ingrese datos invalidos.
+    2. controla que no supere su límite de 20 inscritos.
+    3. Almacena la información de cada persona para que no se mezcle.    
+    4. Revisa la lista de socios para ver si el nombre que escribiste está registrado.
+    5. Suma las edades de todos y calcula la edad promedio del gimnasio.
+    6. Compara los pesos de todos los socios para descubrir quién es el más liviano.
+    7. Toma el peso del socio y calcula cuánta agua debe tomar al día para estar saludable.
+    8. Toma su edad y le sugiere el tipo de ejercicio ideal para su etapa de vida.
+
+Salida: 
+    1. Un menú de opciones.
+    2. Lista de todos los socios que se han registrado hasta el momento.
+    3. Un mensaje "Socio encontrado en la posición tal..." o un aviso si  no está registrado. 
+    4. Cuál es el promedio de edad y quién es el socio de menor peso.    
+    5. Mensaje de cuántos litros de agua necesita al día y qué rutina le conviene hacer.
+
+*/
+
