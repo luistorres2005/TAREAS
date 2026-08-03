d2dfc28f5ad109e8edb14b215091bc62d49765ed
luistorres2005 <luistorres.20199@gmail.com>
2026-08-02 23:05:18 -0500

    Se agrega busqueda secuencial, reporte de promedio y peso minimo

--- miniProyecto/main.cpp
+++ miniProyecto/main.cpp
@@ -47,25 +47,19 @@ Salida:
 
 using namespace std;
 
-// Constante para el límite máximo de socios en el gimnasio 
 const int MAX_SOCIOS = 20;
 
-// === PROTOTIPOS DE LAS FUNCIONES ===
-// Pasamos los arreglos para guardar los datos y el contador por referencia para actualizarlo 
+// === PROTOTIPOS ===
 void registrarSocio(string nombres[], int edades[], double pesos[], int &cantidad);
-// Mostrar la lista de socios 
 void mostrarSocios(const string nombres[], const int edades[], const double pesos[], int cantidad);
+void buscarSocio(const string nombres[], const int edades[], const double pesos[], int cantidad);
+void mostrarReporte(const string nombres[], const int edades[], const double pesos[], int cantidad);
 
 int main() {
-    
     string nombres[MAX_SOCIOS];
     int edades[MAX_SOCIOS];
     double pesos[MAX_SOCIOS];
-
-    // Variable para controlar los socios registrados actualmente
     int cantidad_registros = 0;
-
-    // Variable para capturar la opción del menú
     int opcion;
 
     // MENÚ REPETITIVO
@@ -83,28 +77,26 @@ int main() {
         cout << "Seleccione una opcion (1-6): ";
         cin >> opcion;
 
-        // VALIDACIÓN DE ENTRADA INCOHERENTE         
+        // VALIDACIÓN DE ENTRADA INCOHERENTE
         if (cin.fail()) {
-            cin.clear(); // Limpia el estado de error de cin
-            cin.ignore(10000, '\n'); // Ignora los caracteres incorrectos ingresados
-            opcion = -1; // Forzamos una opción inválida para activar el caso 'default'
+            cin.clear();
+            cin.ignore(10000, '\n');
+            opcion = -1;
         }
 
         // SELECTOR DE ACCIÓN
         switch (opcion) {
             case 1:
-                // Función de registro 
                 registrarSocio(nombres, edades, pesos, cantidad_registros);
                 break;
             case 2:
-                // Función de listado
                 mostrarSocios(nombres, edades, pesos, cantidad_registros);
                 break;
             case 3:
-                cout << "\n[Proximamente] Aqui buscaremos a un socio por su nombre..." << endl;
+                buscarSocio(nombres, edades, pesos, cantidad_registros);
                 break;
             case 4:
-                cout << "\n[Proximamente] Aqui calcularemos el promedio de edad y el socio mas liviano..." << endl;
+                mostrarReporte(nombres, edades, pesos, cantidad_registros);
                 break;
             case 5:
                 cout << "\n[Proximamente] Aqui se ejecutara el Asistente de Hidratacion..." << endl;
@@ -117,27 +109,23 @@ int main() {
                 break;
         }
 
-    } while (opcion != 6); // Se repite hasta que el usuario elija la opción Salir 
+    } while (opcion != 6);
 
     return 0;
 }
 
-// === IMPLEMENTACIÓN DE LA FUNCIÓN DE REGISTRO ===
+// === REGISTRAR NUEVO SOCIO ===
 void registrarSocio(string nombres[], int edades[], double pesos[], int &cantidad) {
-    // Valida si el gimnasio ya está lleno 
     if (cantidad >= MAX_SOCIOS) {
         cout << "\nERROR. El gimnasio esta lleno, no se pueden registrar mas de " << MAX_SOCIOS << " socios." << endl;
         return; 
     }
 
     cout << "\n--- REGISTRAR NUEVO SOCIO ---" << endl;
-    
-    // Captura de nombre
     cout << "Ingrese el nombre completo del socio: ";
     cin.ignore(); 
-    getline(cin, nombres[cantidad]); // leer nombres y apellidos con espacios
+    getline(cin, nombres[cantidad]);
 
-    // Validación de edad 
     int edad_ingresada;
     do {
         cout << "Ingrese la edad (debe ser mayor o igual a 0): ";
@@ -150,9 +138,8 @@ void registrarSocio(string nombres[], int edades[], double pesos[], int &cantida
             break; 
         }
     } while (true);
-    edades[cantidad] = edad_ingresada; // Guardamos en el arreglo de edades en la posición actual
+    edades[cantidad] = edad_ingresada;
 
-    // Validación del Peso 
     double peso_ingresado;
     do {
         cout << "Ingrese el peso en kg: ";
@@ -165,16 +152,14 @@ void registrarSocio(string nombres[], int edades[], double pesos[], int &cantida
             break; 
         }
     } while (true);
-    pesos[cantidad] = peso_ingresado; // Guardamos en el arreglo de pesos en la posición actual
+    pesos[cantidad] = peso_ingresado;
 
-    // Incrementamos la variable de control 
     cantidad++; 
     cout << "\nLISTO. Socio registrado con exito, socios actuales: " << cantidad << endl;
 }
 
-//IMPLEMENTACIÓN DE LA FUNCIÓN DE LISTADO
+// === LISTADO DE SOCIOS ===
 void mostrarSocios(const string nombres[], const int edades[], const double pesos[], int cantidad) {
-    //Validar si no hay socios para evitar recorrer arreglos vacíos
     if (cantidad == 0) {
         cout << "\nERROR. No hay socios registrados todavia." << endl;
         return;
@@ -186,7 +171,6 @@ void mostrarSocios(const string nombres[], const int edades[], const double peso
     cout << "ID\tEdad\tPeso (kg)\tNombre Completo" << endl;
     cout << "-----------------------------------------------------------------" << endl;
     
-    // Recorremos los arreglos paralelos desde 0 hasta cantidad - 1
     for (int i = 0; i < cantidad; i++) {
         cout << (i + 1) << "\t" 
              << edades[i] << "\t" 
@@ -195,4 +179,68 @@ void mostrarSocios(const string nombres[], const int edades[], const double peso
     }
     cout << "=================================================================" << endl;
     cout << "Capacidad actual: " << cantidad << " de " << MAX_SOCIOS << " cupos ocupados." << endl;
+}
+
+// === BÚSQUEDA SECUENCIAL ===
+void buscarSocio(const string nombres[], const int edades[], const double pesos[], int cantidad) {
+    if (cantidad == 0) {
+        cout << "\nERROR. No hay socios registrados para realizar la busqueda." << endl;
+        return;
+    }
+
+    cout << "\n--- BUSCAR SOCIO POR NOMBRE ---" << endl;
+    cout << "Ingrese el nombre del socio que desea buscar: ";
+    string nombre_buscado;
+    cin.ignore();
+    getline(cin, nombre_buscado);
+
+    bool encontrado = false;
+    for (int i = 0; i < cantidad; i++) {
+        if (nombres[i] == nombre_buscado) {
+            cout << "\nLISTO. Socio encontrado!" << endl;
+            cout << "ID en lista: " << (i + 1) << endl;
+            cout << "Nombre completo: " << nombres[i] << endl;
+            cout << "Edad: " << edades[i] << " anos" << endl;
+            cout << "Peso actual: " << pesos[i] << " kg" << endl;
+            encontrado = true;
+            break;
+        }
+    }
+
+    if (!encontrado) {
+        cout << "\nERROR. El socio \"" << nombre_buscado << "\" no se encuentra registrado." << endl;
+    }
+}
+
+// === REPORTE ESTADÍSTICO ===
+void mostrarReporte(const string nombres[], const int edades[], const double pesos[], int cantidad) {
+    if (cantidad == 0) {
+        cout << "\nERROR. No hay datos suficientes para generar el reporte." << endl;
+        return;
+    }
+
+    double suma_edades = 0;
+    for (int i = 0; i < cantidad; i++) {
+        suma_edades += edades[i];
+    }
+    double promedio_edad = suma_edades / cantidad;
+
+    double peso_minimo = pesos [0];
+    int indice_minimo = 0;
+    for (int i = 1; i < cantidad; i++) {
+        if (pesos[i] < peso_minimo) {
+            peso_minimo = pesos[i];
+            indice_minimo = i;
+        }
+    }
+
+    cout << "\n=====================================================" << endl;
+    cout << "                 REPORTE ESTADISTICO                 " << endl;
+    cout << "=====================================================" << endl;
+    cout << "1. Promedio de edad del gimnasio: " << promedio_edad << " anos." << endl;
+    cout << "2. Socio con menor peso registrado actualmente:" << endl;
+    cout << "   - Nombre: " << nombres[indice_minimo] << endl;
+    cout << "   - Peso: " << pesos[indice_minimo] << " kg" << endl;
+    cout << "   - Edad: " << edades[indice_minimo] << " anos" << endl;
+    cout << "=====================================================" << endl;
 }
