24e041ab4b6de73c96d199c75ff759de37b72d6a
luistorres2005 <luistorres.20199@gmail.com>
2026-08-04 18:36:27 -0500

    Ejercicio2

---
+++ tareaClase10/Ejercicio2/src/App.java
@@ -0,0 +1,16 @@
+/*
+Ejercicio 2 — Clase Mascota
+Estudiante: Torres Cabezas Yober Luis
+Fecha: 04/08/2026
+
+Enunciado:
+Crea una clase llamada Telefono que represente un teléfono.
+La clase debe tener los siguientes atributos privados:
+marca, modelo, precio.
+*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
