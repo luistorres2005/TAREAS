36b17b232dcb666b350e757cf795dd6d6de60d35
luistorres2005 <luistorres.20199@gmail.com>
2026-08-04 18:45:14 -0500

    Declaracion de atributos privados de la clase Telefono y creacion de constructor

--- tareaClase10/Ejercicio2/src/Telefono.java
+++ tareaClase10/Ejercicio2/src/Telefono.java
@@ -1,3 +1,12 @@
 public class Telefono {
-    
+    private String marca;
+    private String modelo;
+    private double precio;  
+
+    //Constructor
+    public Telefono(String marca, String modelo, double precio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+    }
 }
