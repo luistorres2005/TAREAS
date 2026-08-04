e78d361e08aa40607394b882b40aa4b45a0bf3a3
luistorres2005 <luistorres.20199@gmail.com>
2026-08-04 16:23:27 -0500

    Creacion de contructor y reordenamiento de carpeta

---
+++ tareaClase10/Ejercicio1/src/Mascota.java
@@ -0,0 +1,12 @@
+public class Mascota {
+    String nombre;
+    String especie;
+    int edad;
+
+    //constructor
+    public Mascota(String nombre, String especie, int edad) {
+        this.nombre = nombre;
+        this.especie = especie;
+        this.edad = edad;
+    }   
+}
