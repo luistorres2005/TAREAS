31480ff45a9e96857d6a391fbbad6d0d4d894a57
luistorres2005 <luistorres.20199@gmail.com>
2026-08-04 16:10:36 -0500

    Creacion del archivo App y de la clase Mascota con sus atributos

---
+++ tareaClase10/App.java/src/App.java
@@ -0,0 +1,12 @@
+/*
+Ejercicio 1 — Clase Mascota
+Estudiante: Torres Cabezas Yober Luis
+Enunciado
+Crea una clase llamada Mascota que represente una mascota sencilla.
+*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+    }
+}
