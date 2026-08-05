e7c1341664769ffefffc7755c3085b7a9ae665fc
luistorres2005 <luistorres.20199@gmail.com>
2026-08-04 19:45:01 -0500

    Se agrega una validacion en el constructor, se crea los getters y setters con una validacion en setter de precio. Creacion de el metodo mostrarInfo()

--- tareaClase10/Ejercicio2/src/Telefono.java
+++ tareaClase10/Ejercicio2/src/Telefono.java
@@ -7,6 +7,51 @@ public class Telefono {
     public Telefono(String marca, String modelo, double precio) {
         this.marca = marca;
         this.modelo = modelo;
-        this.precio = precio;
+
+        //validacion 
+        if(precio >= 0){
+            this.precio = precio;            
+        }else{
+            this.precio = 0;
+        }        
     }
+
+
+    //Getters
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    //Setters
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setPrecio(double precio){
+        if (precio >=0){
+            this.precio = precio;
+        }else{
+            System.out.println("El precio no puede ser negativo.");
+        }
+    }
+
+    //metodo para mostrar informacion
+    public void mostrarInfo() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Precio: $" + precio);
+    }
+
 }
