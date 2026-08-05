31489f8822b2ae8f2b0b752290935c1521daac3b
luistorres2005 <luistorres.20199@gmail.com>
2026-08-04 21:19:00 -0500

    Creacion de objeto telefono, obtenemos informacion, cambiamos datos y mostramos resultados

--- tareaClase10/Ejercicio2/src/App.java
+++ tareaClase10/Ejercicio2/src/App.java
@@ -1,5 +1,5 @@
 /*
-Ejercicio 2 — Clase Mascota
+Ejercicio 2 — Clase telefono
 Estudiante: Torres Cabezas Yober Luis
 Fecha: 04/08/2026
 
@@ -11,6 +11,34 @@ marca, modelo, precio.
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //objeto telefono.
+        Telefono miTelefono = new Telefono("Samsumg", "A15", 250.0);
+
+        System.out.println("===Informacion del Telefono===");
+        System.out.println("Marca: " + miTelefono.getMarca());
+        System.out.println("Modelo: " + miTelefono.getModelo());
+        System.out.println("Precio: $" + miTelefono.getPrecio());
+
+        System.out.println();
+        
+        //modificar datos del telefono
+        miTelefono.setMarca("Apple");
+        System.out.println("Marca modificada: " + miTelefono.getMarca());       
+        System.out.println();
+
+        miTelefono.setModelo("Iphone 14");
+        System.out.println("Modelo modificado: " + miTelefono.getModelo());
+        System.out.println();
+
+        miTelefono.setPrecio(1000.0);
+        System.out.println("Precio modificado: $" + miTelefono.getPrecio());
+        System.out.println();
+
+        System.out.println("--- Prueba de validacion ---");
+        miTelefono.setPrecio(-50.0);
+        System.out.println();
+
+        System.out.println("=== Datos Finales ===");
+        miTelefono.mostrarInfo();
     }
 }
